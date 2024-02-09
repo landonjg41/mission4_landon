055c9cb8b1b61d5803331bf1cb611172ea39c00e
Landon Graham <landonjg41@gmail.com>
2024-02-08 19:16:36 -0700

    Add project files.

---
+++ mission4_landon/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mission4_landon.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ mission4_landon/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// function to calculate cost of services used
+$('#calculate').on('click', function (event) {
+    //declare variables
+    var hours = parseFloat($('#hours').val());
+    var rate = parseFloat($('#rate').val());
+    if (hours > 0) {
+        var total = hours * rate;
+        $('#total').val(total.toFixed(2));
+    } else {
+        alert('Please enter a positive number for hours.');
+        $('#hours').val('').focus();
+    }
+});
+
